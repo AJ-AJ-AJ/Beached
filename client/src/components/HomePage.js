5c1b6df120dc551546d700bd05fb32edae5ad996
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-08-03 11:18:56 -0400

    added styling to landing page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -18,26 +18,45 @@ class HomePage extends Component {
         axios.get('/api/beach')
             .then((response) => {
                 console.log(response.data)
-                return this.setState({beaches:response.data})
-            }) 
+                return this.setState({ beaches: response.data })
+            })
             .then(() => {
-                let beach = this.state.beaches.filter((beach)=> {
+                let beach = this.state.beaches.filter((beach) => {
                     return beach.name.toUpperCase() === 'BEACH'
                 })
                 beachId = `/beach/:beach_id`
             })
     }
 
-        render(){
-            return(
+    render() {
+        return (
+            <div>
                 <div>
-                    <h1>ello</h1>
-                    <a href="/beach/:beach_id" alt="">
-                    Beach
-                    </a>
+                    <h1>Beached.</h1>
+                    <img className='landingImg' src="https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5222667852001_5214854492001-vs.jpg?pubId=5104226627001&videoId=5214854492001" alt="jamaica" />
                 </div>
-            )
-        }
-    }
+                <a href="/beach/:beach_id">
+                    <div className='landingDiv'>
+                        <div className='photoDiv1'>
+                            <h2>JA</h2>
+                        </div>
+                        <div className='photoDiv2'>
+                            <h2>FIJI</h2>
+                        </div>
+                    </div>
 
-    export default HomePage;
+                    <div className='landingDiv'>
+                        <div className='photoDiv2'>
+                            <h2>CUBA</h2>
+                        </div>
+                        <div className='photoDiv1'>
+                            <h2>DR</h2>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
